29fc48eb835058b487ea0b587ad404baef54b90d
karanja03 <wambuik03@gmail.com>
2023-08-28 11:09:17 +0300

    Updated customer file

--- lib/customer.py
+++ lib/customer.py
@@ -1,56 +1,80 @@
-from review import Review
 
+    
 
+from review import Review  
 
 class Customer:
     all_customers = []
 
     def __init__(self, given_name, family_name):
-        self.given_name = given_name
-        self.family_name = family_name
-        self.reviews=[] # stores the reviews of that particular client that has been initialized and added
+        self._given_name = given_name  
+        self._family_name = family_name
+        self.reviews = []
         Customer.all_customers.append(self)
 
     def given_name(self):
-        return self.given_name
+        return self._given_name
 
-    def set_givenname(self, given_name):
-        self.given_name = given_name
+    def set_given_name(self, given_name):
+        self._given_name = given_name
 
-    def familyname(self):
-        return self.family_name
+    def family_name(self):
+        return self._family_name
 
-    def set_familyname(self, family_name):
-        self.family_name = family_name
+    def set_family_name(self, family_name):
+        self._family_name = family_name
 
-    def fullname(self):
-        return f"{self.given_name} {self.family_name}"
-    
+    def full_name(self):
+        return f"{self._family_name} {self._given_name}"
+
     def get_reviews(self):
         return self.reviews
-    
-      
-    def get_restaurants(self):# to gives a list of restaurants a client given reviews to
-        restaurants = set() # created an empty set of resturants ensuring each restuarnt reviewed by a client is unique
-        for review in self.reviews:#check and iterate all reviews amde by this client
-            restaurants.add(review.get_restaurant())
+
+    def get_restaurants(self):
+        restaurants = set()
+        for review in self.reviews:
+            restaurants.add(review.restaurant())
         return list(restaurants)
 
     def add_review(self, restaurant, rating):
         review = Review(self, restaurant, rating)
-        self.reviews.append(review)# tis two lines link back to he new custoer that hass been created and takes the review associated with the customer and it append the new review to the list of reveiws a client has made
+        self.reviews.append(review)
         restaurant.reviews.append(review)
 
-    def  num_reviews(self):
+    def num_reviews(self):
         return len(self.reviews)
-    # to be completed
-    def find_by_name(self,name):
-        self.name=full_name()
 
+    @classmethod
+    def find_by_name(cls, name):
+        for customer in cls.all_customers:
+            full_name = customer.full_name()
+            if full_name == name:
+                return customer
+        return None
 
+    @classmethod
+    def find_all_by_given_name(cls, name):
+        matching_customers = []
+        for customer in cls.all_customers:
+            if customer._given_name == name:
+                matching_customers.append(customer)
+        return matching_customers
 
     @classmethod
     def all(cls):
         return cls.all_customers
-    
-    
+
+customer1 = Customer("Wambui", "karanja")
+customer2 = Customer("Enoch", "Njoroge")
+customer3 = Customer("John", "kariuki")
+
+found_customer = Customer.find_by_name("John kariuki")
+if found_customer:
+    print(f"Found: {found_customer.full_name()}")
+else:
+    print("Customer not found")
+
+all_johns = Customer.find_all_by_given_name("Enoch")
+for customer in all_johns:
+    print(f"Found: {customer.full_name()}")
+
